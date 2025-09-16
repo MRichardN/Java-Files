e176d2103aebf0d774eead29aa554d55845ae803
Mathenge Richard <40102996+MRichardN@users.noreply.github.com>
2025-09-16 15:00:21 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int age = 10;
+        String name = "Boaz";
+        float sum = 3.021f;
+        char grade = 'A';
+
+        System.out.println("Hello World");
+        System.out.println("Welcome to Java Programming ");
+        System.out.println("Age is "+ age);
+        System.out.println("Name is "+ name);
+        System.out.println("Sum is "+ sum);
+        System.out.println("Grade is "+ grade);
+
+
+
+    }
+}
